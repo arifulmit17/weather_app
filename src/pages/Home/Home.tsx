4de7f56ba0fe34a27ab91d0c54a36d783b1f1c6b
Ariful <arifulmit17@gmail.com>
2025-10-28 22:47:01 +0600

    added shadcn

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@/components/ui/button.js';
 import React from 'react';
 
 const Home = () => {
     return (
         <div>
             This is weather app
+            <Button variant={'outline'}>weather</Button>
         </div>
     );
 };
